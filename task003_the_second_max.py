5cb697a4c089d55102013ea240861e85996a7ef4
maxim_stoma <maksim.stoma@mail.ru>
2022-07-13 23:33:28 +0300

    в решении задачи 3 учтён вывод второго по величине элемента при наличии нескольких максимальных

--- task003_the_second_max.py
+++ task003_the_second_max.py
@@ -3,4 +3,8 @@ array = (random.sample(range(99), 10))
 array.append(0)
 print(f'Исходный массив {array}')
 sorted_array = sorted(array, reverse=True)
-print(f'Второй по величине элемент массива: {sorted_array[1]}')
+second_max = sorted_array[1]
+for i in range(len(sorted_array)):
+    if sorted_array[0] == sorted_array[i]:
+        second_max = sorted_array[i + 1]
+print(f'Второй по величине элемент массива: {second_max}')
